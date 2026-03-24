2368570f6c40cbf1a641e00e1bd628dbb7000490
Nikita Shinde <nikitashinde1705@gmail.com>
2026-03-25 01:32:54 +0530

    added changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
 const API = axios.create({
   //baseURL: "http://localhost:5000/api"
   baseURL: import.meta.env.VITE_API_URL
+  
 });
 
 // Add token automatically
